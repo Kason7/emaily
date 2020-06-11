29937c96caf7dd8d039a4b3a96907295636d0004
Kasper Sonne <kaspersonne27@gmail.com>
2020-06-11 21:22:47 +0200

    Testing Heroku push through Github

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
-// prod.js keys for production environment, to match with Heroku variables
+// prod.js keys for production environment, to set with Heroku variables
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
