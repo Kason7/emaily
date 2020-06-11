5891f01227eb00a67df147b76905c52db2de8076
Kasper Sonne <kaspersonne27@gmail.com>
2020-06-11 21:12:20 +0200

    Section 5: Dev vs Prod Environments

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// prod.js keys for production environment, to match with Heroku variables
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express'); // Getting our underlying app library, on top of nodejs
+const mongoose = require('mongoose'); // Library to handle database template configration and communication
 const cookieSession = require('cookie-session'); // Used to encrypt the oauth cookies
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
+// Connecting to Database using Mongoose
 mongoose.connect(keys.mongoURI);
 
+// Defining underlying express as app framework
 const app = express();
 
+// Telling express to consider our Middleware
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, // Has to be given in miliseconds, which is equal to 30 days
@@ -19,7 +22,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Setting our routes to our express app
 require('./routes/authRoutes')(app);
 
+// Defining port and tell app to listen to incoming requests
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
