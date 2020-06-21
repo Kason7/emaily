ce58b8bef0a41a7cd68887de811211904afb4a6e
Kasper Sonne <kaspersonne27@gmail.com>
2020-06-21 10:26:24 +0200

    Further prod deploy debug

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ app.use(passport.session());
 // Setting our routes to our express app
 require('./routes/authRoutes')(app);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Defining port and tell app to listen to incoming requests
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
